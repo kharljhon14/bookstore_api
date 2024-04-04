48a2cb37361bdb05d34a88b2c80853b4787b8ef3
kharle14 <enriquezkharl14@gmail.com>
2024-04-05 03:57:42 +0800

    Req & res data types

--- src/controllers/auth.rs
+++ src/controllers/auth.rs
@@ -1,11 +1,46 @@
+use rocket::{
+    serde::{json::Json, Deserialize, Serialize},
+    State,
+};
+use sea_orm::DatabaseConnection;
+
 use super::Response;
 
-#[post("/sign-in")]
-pub async fn sign_in() -> Response<String> {
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct ReqSignIn {
+    email: String,
+    password: String,
+}
+
+#[derive(Deserialize, Serialize, Responder)]
+#[serde(crate = "rocket::serde")]
+pub struct ResSignIn {
+    token: String,
+}
+
+#[post("/sign-in", data = "<req_sign_in>")]
+pub async fn sign_in(
+    db: &State<DatabaseConnection>,
+    req_sign_in: Json<ReqSignIn>,
+) -> Response<ResSignIn> {
+    let db = db as &DatabaseConnection;
     todo!()
 }
 
-#[post("/sign-up")]
-pub async fn sign_up() -> Response<String> {
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct ReqSignUp {
+    email: String,
+    password: String,
+    firstname: Option<String>,
+    lastname: Option<String>,
+}
+
+#[post("/sign-up", data = "<req_sign_up>")]
+pub async fn sign_up(
+    db: &State<DatabaseConnection>,
+    req_sign_up: Json<ReqSignUp>,
+) -> Response<String> {
     todo!()
 }
--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,7 @@ async fn rocket() -> _ {
 
     rocket::build()
         .attach(CORS)
+        .manage(db)
         .mount("/", routes![options])
         .mount("/", routes![index])
         .mount(
