8b91bc26f09b7a3b73150734c50a5ce914b6f618
kharle14 <enriquezkharl14@gmail.com>
2024-04-09 20:38:09 +0800

    added authors index route

--- src/controllers/author.rs
+++ src/controllers/author.rs
@@ -1,8 +1,60 @@
-use super::Response;
+use rocket::{
+    http::Status,
+    serde::{json::Json, Serialize},
+    State,
+};
+use sea_orm::{DatabaseConnection, EntityTrait, QueryOrder};
+
+use crate::{
+    auth::AuthenticatedUser,
+    entities::{author, prelude::*},
+};
+
+use super::{Response, SuccessResponse};
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct ResAuthor {
+    id: i32,
+    firstname: String,
+    lastname: String,
+    bio: String,
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+pub struct ResAuthorList {
+    total: usize,
+    authors: Vec<ResAuthor>,
+}
 
 #[get("/")]
-pub async fn index() -> Response<String> {
-    todo!()
+pub async fn index(
+    db: &State<DatabaseConnection>,
+    _user: AuthenticatedUser,
+) -> Response<Json<ResAuthorList>> {
+    let db = db as &DatabaseConnection;
+
+    let authors = Author::find()
+        .order_by_desc(author::Column::UpdatedAt)
+        .all(db)
+        .await?
+        .iter()
+        .map(|author| ResAuthor {
+            id: author.id,
+            firstname: author.firstname.to_owned(),
+            lastname: author.lastname.to_owned(),
+            bio: author.bio.to_owned(),
+        })
+        .collect::<Vec<_>>();
+
+    Ok(SuccessResponse((
+        Status::Ok,
+        Json(ResAuthorList {
+            total: authors.len(),
+            authors,
+        }),
+    )))
 }
 
 #[post("/")]
